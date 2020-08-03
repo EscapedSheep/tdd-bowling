962d4da21acb79ac41b6868389cec67e16a1c57a
EscapedSheep <sfgmdf001@126.com>
2020-08-03 18:04:16 +0800

    [maida Ge] feat: get the score of pin and spare given bowls o each throws

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,22 +1,26 @@
 public class BowlingGame {
     private int[][] bowls;
-    private int[] score;
+    private int[] scores;
 
     public BowlingGame(int[][] bowls) {
         this.bowls = bowls;
-        this.score = new int[10];
+        this.scores = new int[10];
     }
 
     public int[] getScore() {
         calculateScore();
-        return score;
+        return scores;
     }
 
     private void calculateScore() {
-        for (int i = 0; i < bowls.length; i++) {
-            score[i] = bowls[i][0] + bowls[i][1];
+        for (int i = 0; i < 10; i++) {
+            int tempScore = bowls[i][0] + bowls[i][1];
+            scores[i] = tempScore;
+            if (tempScore == 10)
+                scores[i] += bowls[i+1][0];
         }
     }
+
 
 
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,10 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameTest {
     @Test
-    void should_get_score_of_bowls_in_one_frame_when_no_strike_nor_spare_given_twice_throws() {
-        int[][] bowls = new int[][]{{5,6}, {3,5}, {2,7},{8,8},{4,9},{3,2},{1,1},{2,4},{2,5},{3,5}};
+    void should_get_score_when_pin_given_bowls_of_each_throws() {
+        int[][] bowls = new int[][]{{5,3}, {3,5}, {2,7},{8,1},{4,2},{3,2},{1,1},{2,4},{2,5},{3,5}};
         BowlingGame bowlingGame = new BowlingGame(bowls);
         int[] score = bowlingGame.getScore();
-        assertArrayEquals(new int[]{11,8,9,16,13,5,2,6,7,8}, score);
+        assertArrayEquals(new int[]{8,8,9,9,6,5,2,6,7,8}, score);
+    }
+
+    @Test
+    void should_get_score_when_pin_and_spare_given_bowls_of_each_throws() {
+        int[][] bowls = new int[][]{{5,5}, {3,5}, {2,7},{8,2},{4,1},{3,7},{1,1},{2,4},{2,5},{3,7},{1}};
+        BowlingGame bowlingGame = new BowlingGame(bowls);
+        int[] score = bowlingGame.getScore();
+        assertArrayEquals(new int[]{13,8,9,14,5,11,2,6,7,11}, score);
     }
 }
