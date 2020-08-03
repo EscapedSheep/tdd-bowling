02dd727918e51ab3a783ae7b2ab89746e16edd31
EscapedSheep <sfgmdf001@126.com>
2020-08-03 16:20:55 +0800

    [maida Ge] feat: get the score of pins in one frame when no strike nor spare

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,5 @@
+public class BowlingGame {
+    public int getOneFrameScore(int first, int second) {
+        return first + second;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BowlingGameTest {
+    @Test
+    void should_get_score_of_pins_in_one_frame_when_no_strike_nor_spare() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int score = bowlingGame.getOneFrameScore(5, 6);
+        assertEquals(11, score);
+    }
 }
