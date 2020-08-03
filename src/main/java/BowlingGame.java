147a491b7d40574bfa9b8ba97b336d7978fb1c14
EscapedSheep <sfgmdf001@126.com>
2020-08-03 16:58:55 +0800

    [maida Ge] refactor get socre of bowls when pin

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,5 +1,22 @@
 public class BowlingGame {
-    public int getOneFrameScore(int first, int second) {
-        return first + second;
+    private int[][] bowls;
+    private int[] score;
+
+    public BowlingGame(int[][] bowls) {
+        this.bowls = bowls;
+        this.score = new int[10];
     }
+
+    public int[] getScore() {
+        calculateScore();
+        return score;
+    }
+
+    private void calculateScore() {
+        for (int i = 0; i < bowls.length; i++) {
+            score[i] = bowls[i][0] + bowls[i][1];
+        }
+    }
+
+
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,9 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameTest {
     @Test
-    void should_get_score_of_pins_in_one_frame_when_no_strike_nor_spare() {
-        BowlingGame bowlingGame = new BowlingGame();
-        int score = bowlingGame.getOneFrameScore(5, 6);
-        assertEquals(11, score);
+    void should_get_score_of_bowls_in_one_frame_when_no_strike_nor_spare_given_twice_throws() {
+        int[][] bowls = new int[][]{{5,6}, {3,5}, {2,7},{8,8},{4,9},{3,2},{1,1},{2,4},{2,5},{3,5}};
+        BowlingGame bowlingGame = new BowlingGame(bowls);
+        int[] score = bowlingGame.getScore();
+        assertArrayEquals(new int[]{11,8,9,16,13,5,2,6,7,8}, score);
     }
 }
